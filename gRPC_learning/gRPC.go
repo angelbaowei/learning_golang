e892a6755605a5289a3e73c18cc1fc927794f5ff
angelbaowei <angelbaowei@163.com>
2021-04-15 21:33:40 +0800

    gRPC

---
+++ gRPC_learning/gRPC.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+	/*
+	gRPC开发同样分三步：
+
+	编写.proto文件，生成指定语言源代码。
+	编写服务端代码
+	编写客户端代码
+	 */
+
+	/*
+	gRPC是基于Protocol Buffers。
+	Protocol Buffers是一种与语言无关，平台无关的可扩展机制，用于序列化结构化数据。
+	使用Protocol Buffers可以一次定义结构化的数据，然后可以使用特殊生成的源代码轻松地在各种数据流中
+	使用各种语言编写和读取结构化数据。
+	 */
+
+
+
+}
